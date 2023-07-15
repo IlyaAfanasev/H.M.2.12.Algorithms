75f5875251fa7c037e312d92d7ac26d765832f46
IlyuAfanasev <gecktor.86@mail.ru>
2023-07-15 20:47:46 +0400

    adjusting array initialization

--- src/main/java/pro/sky/java/course2/Main.java
+++ src/main/java/pro/sky/java/course2/Main.java
@@ -5,26 +5,26 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-//
-//        Integer [] arrayList1 = generateRandomArray();
-//        Integer [] arrayList2 = generateRandomArray();
-//        Integer [] arrayList3 = generateRandomArray();
-//
-//
-//
-//        long startArrays1 = System.currentTimeMillis();
-//        sortInsertion(arrayList1);
-//        System.out.println(System.currentTimeMillis() - startArrays1);
-//
-//
-//        long startArrays2 = System.currentTimeMillis();
-//        sortSelection(arrayList2);
-//        System.out.println(System.currentTimeMillis() - startArrays2);
-//
-//
-//        long startArrays3 = System.currentTimeMillis();
-//        sortBubble(arrayList3);
-//        System.out.println(System.currentTimeMillis() - startArrays3);
+
+        Integer [] arrayList1 = generateRandomArray();
+        Integer[] arrayList2 = Arrays.copyOf(arrayList1, arrayList1.length);
+        Integer [] arrayList3 =Arrays.copyOf(arrayList1, arrayList1.length);
+
+
+
+        long startArrays1 = System.currentTimeMillis();
+        sortInsertion(arrayList1);
+        System.out.println(System.currentTimeMillis() - startArrays1);
+
+
+        long startArrays2 = System.currentTimeMillis();
+        sortSelection(arrayList2);
+        System.out.println(System.currentTimeMillis() - startArrays2);
+
+
+        long startArrays3 = System.currentTimeMillis();
+        sortBubble(arrayList3);
+        System.out.println(System.currentTimeMillis() - startArrays3);
 
 
 
